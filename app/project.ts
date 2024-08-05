dfac52d03f5427c093162beccbb6640c3002f20e
Henry <henrytran012599@gmail.com>
2024-08-05 15:30:12 +0700

    cmfinal2

--- app/project.ts
+++ app/project.ts
@@ -8,17 +8,23 @@ export const project = [
     description:
       "This project is a clone of the Shopee website, a leading e-commerce platform in Asia. The Shopee API for this project isprovided by TMAPI. Key functionalities include searching for keywords, adding items, and checking out items",
     image: shopee,
+    gitHub: "https://github.com/HenryTran0125/Shopee",
+    visitPage: "https://shopeeclone-nine.vercel.app/",
   },
   {
     name: "Ordering Pizza",
     description:
       "This project is a pizza ordering system that begins by having the user enter their name. Following this, users can select the pizza they wish to order, provide their delivery address, and complete the order.",
     image: pizza,
+    gitHub: "https://github.com/HenryTran0125/Pizza",
+    visitPage: "https://pizza-five-omega.vercel.app/",
   },
   {
     name: "The Wild Oasis",
     description:
       "This project is a hotel booking system designed to facilitate reservations from clients, where the primary user is an employer. Bookings are confirmed when clients arrive at the hotel. The system’s main functions include monitoring statistical activities and managing all aspects of bookings—whether unconfirmed, checked-in, or checked-out. It also allows for the management of cabin availability and the creation of new user accounts. Additionally, the system features a dark mode option for enhanced usability ",
     image: wildOasis,
+    gitHub: "https://github.com/HenryTran0125/The-Wild-Oasis",
+    visitPage: "https://the-wild-oasis-gold.vercel.app/login",
   },
 ];
